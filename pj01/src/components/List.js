564f2dd8b6308371f38789d7483a1a64f492e556
Carlos Filipe <lordcfprogramador@gmail.com>
2023-10-07 16:01:41 -0300

    Aula #8
    Aprendendo sobre Props

--- pj01/src/components/List.js
+++ pj01/src/components/List.js
@@ -5,9 +5,9 @@ function List() {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
-                <Item marca="Lamborgini"/>
+                <Item marca="Ferrari" ano_lancamento={2023} />
+                <Item marca="Fiat" ano_lancamento={2023} />
+                <Item marca="Lamborghini" />
             </ul>
         </>
     )
