0e8c0051d26404900b6e0c63a9d778df6e60e010
Carlos Filipe <lordcfprogramador@gmail.com>
2023-10-07 13:58:28 -0300

    Aula #4

--- pj01/src/App.js
+++ pj01/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import HelloWord from './components/HelloWord';
 
 function App() {
 
     const name = 'Carlos Filipe'.toLocaleUpperCase()
     const url = "https://via.placeholder.com/150"
 
-    function sum (a, b) {
+    function sum(a, b) {
         return a + b
     }
 
@@ -15,6 +16,7 @@ function App() {
             <p>Olá, {name}</p>
             <p>Soma: {sum(2, 3)}</p>
             <img src={url} alt="Minha imagem" />
+            <HelloWord />
         </div>
     );
 }
---
+++ pj01/src/components/Frase.js
@@ -0,0 +1,10 @@
+function Frase() {
+
+    return (
+        <div>
+            <p>Criando uma frase</p>
+        </div>
+    );
+}
+
+export default Frase;
