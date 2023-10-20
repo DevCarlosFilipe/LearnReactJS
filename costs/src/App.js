f45eadb773cee31c209438d77c589dab036ad175
Carlos Filipe <lordcfprogramador@gmail.com>
2023-10-20 13:03:56 -0300

    reestruturando

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import Container from "./components/layout/Container";
+
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+
 function App() {
     return (
-        <div>
-            <p> Costs</p>
-        </div>
+        <Router>
+            <NavBar />
+
+            <Container customClass="min-height">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/company" element={<Company />} />
+                    <Route path="/newproject" element={<NewProject />} />
+                </Routes>
+            </Container>
+
+            <Footer />
+        </Router>
     );
 }
 
